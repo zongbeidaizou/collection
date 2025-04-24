63d670f743f9b5868fd1b8cfc9b2053c3c3e4452
1801 <781021164@qq.com>
2025-04-24 21:50:43 +0800

    -

---
+++ lib/providers/refresh_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../models/collection_order_entity.dart';
+
+
+class RefreshProvider extends ChangeNotifier {
+
+  bool _orderRefresh = false;
+  bool get orderRefresh => _orderRefresh;
+  bool _logRefresh = false;
+  bool get logRefresh => _logRefresh;
+  bool _bonusRefresh = false;
+  bool get bonusRefresh => _bonusRefresh;
+  int _bonusNewCount = 0;
+  int get bonusNewCount => _bonusNewCount;
+  bool _newsRefresh = false;
+  bool get newsRefresh => _newsRefresh;
+  bool _homeRefresh = false;
+  bool get homeRefresh => _homeRefresh;
+
+  void setUserEntity(CollectionOrderOther userEntity) {
+    if(userEntity.profile!.aGCollectionCommissionNewCount != _bonusNewCount){
+      _bonusNewCount = userEntity.profile!.aGCollectionCommissionNewCount!;
+      _bonusRefresh = true;
+      notifyListeners();
+    }
+
+  }
+
+
+}
+
